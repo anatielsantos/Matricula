e4c400362084abf806ea1cd14a9e0622850b06ea
anatielsantos <anatielsantos@gmail.com>
2011-09-28 16:30:01 -0300

    botao de upload de arquivos colocado no uploadXml.zul

--- grails-app/composers/matricula/UploadXmlComposer.groovy
+++ grails-app/composers/matricula/UploadXmlComposer.groovy
@@ -3,8 +3,14 @@ package matricula
 import org.zkoss.zkgrails.*
 
 class UploadXmlComposer extends GrailsComposer {
+	Fileupload arquivoUpload
 
     def afterCompose = { window ->
         // initialize components here
     }
+	
+	def onUpload_arquivoUpload() {
+		arquivoUpload.
+		alert("Upload XML")
+	} 
 }
